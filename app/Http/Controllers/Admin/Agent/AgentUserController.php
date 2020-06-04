04439dcfc503a2bc5dd26fba6d52cc9d63552565
UltrmanOrbr <1216281190@qq.com>
2020-06-04 16:07:29 +0800

    orb 主播更改

--- app/Http/Controllers/Admin/Agent/AgentUserController.php
+++ app/Http/Controllers/Admin/Agent/AgentUserController.php
@@ -84,6 +84,7 @@ class AgentUserController extends Controller
             unset($data['pwd']);
             unset($data['pwd_confirmation']);
         }
+        dump($data);
         $data['fee']=json_encode($data['fee']);
         $data['limit']=json_encode($data['limit']);
         $data['bjlbets_fee']=json_encode($data['bjlbets_fee']);
