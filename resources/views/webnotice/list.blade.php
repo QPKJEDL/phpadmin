889175ff2ec9d2c29589c78b0b54f21700b8d75f
UltrmanOrbr <1216281190@qq.com>
2020-06-05 14:52:46 +0800

    orb 主播更改

--- resources/views/webnotice/list.blade.php
+++ resources/views/webnotice/list.blade.php
@@ -24,7 +24,7 @@
         </div>
     </div>
     <div class="layui-form-item layui-form-text">
-        <label class="layui-form-label" style="width: 100px">公告内容：</label>
+        <label class="layui-form-label">公告内容：</label>
         <div class="layui-input-block">
             <textarea placeholder="请输入内容" id="content" name="content" class="layui-textarea" style="resize: none">{{$info['content']}}</textarea>
         </div>
