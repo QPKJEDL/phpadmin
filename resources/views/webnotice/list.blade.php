9f28766727b7a9da4a13e760da0446c3c513031f
UltrmanOrbr <1216281190@qq.com>
2020-06-05 15:23:14 +0800

    orb 公告更改

--- resources/views/webnotice/list.blade.php
+++ resources/views/webnotice/list.blade.php
@@ -59,8 +59,10 @@
                     dataType:"json",
                     success:function (res) {
                         if(res.status==1){
-                            layer.msg(res.msg,{icon:6});
-                            window.location.reload();
+                            layer.msg(res.msg,{icon:6,time:2000},function () {
+                                window.location.reload();
+                            });
+
                         }else{
                             layer.msg(res.msg,{shift: 6,icon:5});
                         }
